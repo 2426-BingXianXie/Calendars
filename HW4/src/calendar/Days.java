949d1a602ebc69bd592bf8e844a733680a434b8b
2426-BingXianXie <xie.bingx@northeastern.edu>
2025-05-30 22:47:33 -0400

    structure setup

--- HW4/src/calendar/Days.java
+++ HW4/src/calendar/Days.java
@@ -15,4 +15,25 @@ public enum Days {
 
     this.symbol = symbol;
   }
+
+  /**
+   * Returns the symbol representing the day of the week.
+   * @return the symbol for the day
+   */
+  public char getSymbol() {
+    return symbol;
+  }
+
+  /**
+   * Returns the day of the week corresponding to the given symbol.
+   * @param c the character representing the day
+   * @return the corresponding Days enum value
+   * @throws IllegalArgumentException if the character does not match any day
+   */
+  public static Days fromSymbol(char c) {
+    for (Days day : values()) {
+      if (day.symbol == c) return day;
+    }
+    throw new IllegalArgumentException("Invalid symbol: " + c);
+  }
 }
