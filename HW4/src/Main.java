758d6b672558f44641c1f625c833a9337d485b2c
joshuachann <chan.jos@northeastern.edu>
2025-06-04 15:19:10 -0400

    controller impl

--- HW4/src/Main.java
+++ HW4/src/Main.java
@@ -1,5 +1,6 @@
 import java.io.InputStreamReader;
 import calendar.CalendarController;
+import calendar.CalendarException;
 import calendar.CalendarView;
 import calendar.ICalendar;
 import calendar.ICalendarController;
@@ -9,7 +10,7 @@ import calendar.VirtualCalendar;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws CalendarException {
     ICalendar model = new VirtualCalendar();
     Readable rd = new InputStreamReader(System.in);
     Appendable ap = System.out;
